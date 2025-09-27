d1355a9efba315978639cf4d85bd1c147289fc6f
ismaele-negri <ismaele.negri@mail.polimi.it>
2025-09-27 23:32:26 +0200

    Add files via upload

---
+++ progettoRL_2025/progettoRL_2025.srcs/sources_1/new/outputRegister.vhd
@@ -0,0 +1,65 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 09/24/2025 11:32:03 PM
+-- Design Name: 
+-- Module Name: inputRegister - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+--it is very similar to the input registe
+
+entity outputRegister is
+    generic(
+        length : integer := 8);
+
+    Port ( clk,rst : in STD_LOGIC;
+           dIn : in STD_LOGIC_VECTOR (1 downto 0);
+           dOut : out STD_LOGIC_VECTOR (1 downto 0));
+end outputRegister;
+
+architecture Behavioral of outputRegister is
+    --temporary vector "for all the values"
+    signal rData : std_logic_vector(length-1 downto 0);
+    
+begin
+
+  process(clk, rst)
+  begin
+    --check reset
+    if rst = '1' then
+      rData <= (others => '0');
+    --check if rising_edge
+    elsif rising_edge(clk) then
+      rData <= dIn;
+    end if;
+  end process;
+    
+  --set all the data in rData to the out door of the register 
+  dOut <= rData;
+
+end Behavioral;
