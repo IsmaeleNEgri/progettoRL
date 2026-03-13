3370305f5395a891590ba0cd685bc980df8d8919
ismaele-negri <ismaele.negri@mail.polimi.it>
2026-03-13 21:00:46 +0100

    Update memory.vhd

--- perGithub/sorgenti/memory.vhd
+++ perGithub/sorgenti/memory.vhd
@@ -5,16 +5,18 @@ entity memory is
     generic (
         DATA_WIDTH : integer := 8;
         STACK_DEPTH : integer := 8;
-        STACK_PTR_DEPTH : integer := 3
+        STACK_PTR_DEPTH : integer := 4
     );
     port(
         clk : in std_logic;
         rst : in std_logic;
-        clear : in std_logic;
-
+        
+        pop : in std_logic;
         push : in std_logic;
-        pop  : in std_logic;
-        sp   : in std_logic_vector(STACK_PTR_DEPTH-1 downto 0);
+        invalidate : in std_logic;
+
+        w_addr : in std_logic_vector(2 downto 0);
+        r_addr : in std_logic_vector(2 downto 0);
 
         dIN  : in std_logic_vector(DATA_WIDTH-1 downto 0);
         dOUT : out std_logic_vector(DATA_WIDTH-1 downto 0)
@@ -25,18 +27,22 @@ architecture Behavioral of memory is
 
     type mStack is array (0 to STACK_DEPTH-1) of std_logic_vector(DATA_WIDTH-1 downto 0);
     signal mSig : mStack := (others => (others => '0'));
+    signal dOUT_reg : std_logic_vector(7 downto 0) := (others => '0');
+    signal push_k : std_logic := '0';
+    signal pop_k : std_logic := '0';
 
 begin
-
+    
     process(clk)
     begin
-        if rising_edge(clk) then
+        
+        if rst = '1' then
+            mSig <= (others => (others => '0'));
+    
+        elsif rising_edge(clk)then
 
-            if clear = '1' then
-                mSig <= (others => (others => '0'));
-
-            elsif push = '1' then
-                case sp is
+            if (push = '1' and invalidate = '0') then
+                case w_addr is
                     when "000" => mSig(0) <= dIN;
                     when "001" => mSig(1) <= dIN;
                     when "010" => mSig(2) <= dIN;
@@ -47,22 +53,23 @@ begin
                     when "111" => mSig(7) <= dIN;
                     when others => null;
                 end case;
-
-            elsif pop = '1' then
-                case sp is
-                    when "000" => dOUT <= mSig(0);
-                    when "001" => dOUT <= mSig(1);
-                    when "010" => dOUT <= mSig(2);
-                    when "011" => dOUT <= mSig(3);
-                    when "100" => dOUT <= mSig(4);
-                    when "101" => dOUT <= mSig(5);
-                    when "110" => dOUT <= mSig(6);
-                    when "111" => dOUT <= mSig(7);
-                    when others => dOUT <= (others => '0');
+            
+            elsif (pop  = '1' and invalidate = '0' )then
+               case r_addr is
+                    when "000" => dOUT_reg <= mSig(0);
+                    when "001" => dOUT_reg <= mSig(1);
+                    when "010" => dOUT_reg <= mSig(2);
+                    when "011" => dOUT_reg <= mSig(3);
+                    when "100" => dOUT_reg <= mSig(4);
+                    when "101" => dOUT_reg <= mSig(5);
+                    when "110" => dOUT_reg <= mSig(6);
+                    when "111" => dOUT_reg <= mSig(7);
+                    when others => dOUT_reg <= (others => '0');
                 end case;
+                
             end if;
-
-        end if;
+         end if;
     end process;
-
+    
+    dOUT <= dOUT_reg;
 end Behavioral;
