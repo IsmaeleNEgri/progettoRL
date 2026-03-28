cd7dc81e883937318c79586d796a558dfec2eea2
ismaele-negri <ismaele.negri@mail.polimi.it>
2026-03-28 17:47:10 +0100

    Update stack.vhd

--- IMPORTANT_FILES/sources_1/new/stack.vhd
+++ IMPORTANT_FILES/sources_1/new/stack.vhd
@@ -10,11 +10,11 @@ entity stack is
     port( 
         clk : in std_logic;
         rst : in std_logic;
-        pop : in  std_logic;
-        push : in std_logic;
-        clear : in std_logic;
+        POP : in  std_logic;
+        PUSH : in std_logic;
+        CLEAR : in std_logic;
 
-        dIN : in std_logic_vector(DATA_WIDTH-1 downto 0);
+        DIN : in std_logic_vector(DATA_WIDTH-1 downto 0);
         dOUT : out std_logic_vector(DATA_WIDTH-1 downto 0);
 
         pushError : out std_logic;
@@ -26,7 +26,13 @@ entity stack is
 end stack;
 
 architecture Behavioral of stack is
-
+    
+    signal pop_s : std_logic;
+    signal push_s : std_logic;
+    signal clear_s : std_logic;
+    signal dIN_s : std_logic_vector(DATA_WIDTH-1 downto 0);
+    
+    
     signal sp,spNext : std_logic_vector(STACK_PTR_DEPTH-1 downto 0);
     signal do_push, do_pop, Cout : std_logic;
     signal push_to_confirm, pop_to_confirm: std_logic;
@@ -34,16 +40,31 @@ architecture Behavioral of stack is
     
 
 begin
-
+    
+    sync_in : entity work.syncer
+    port map(
+        clk => clk,
+        rst => rst,
+        pop_i => POP,
+        push_i => PUSH,
+        clear_i => CLEAR,
+        din => DIN,
+        
+        push_s => push_s,
+        pop_s => pop_s,
+        din_s => dIN_s,
+        clear_s => clear_s
+    );
+    
     Push_Pop_Selector : entity work.Push_Pop_Selector
         generic map(STACK_PTR_DEPTH => STACK_PTR_DEPTH)
         port map(
             clk => clk,
             rst => rst,
             
-            clear => clear,
-            push => push,
-            pop => pop,
+            clear => clear_s,
+            push => push_s,
+            pop => pop_S,
             isFullBuffer => isFullBuffer,
             isEmptyBuffer => isEmptyBuffer,
             
@@ -53,8 +74,7 @@ begin
             do_push => do_push,
             do_pop => do_pop,
             push_to_confirm => push_to_confirm,
-            pop_to_confirm => pop_to_confirm
-            
+            pop_to_confirm => pop_to_confirm           
         );
 
     
@@ -73,7 +93,7 @@ begin
             sp => sp,
             isFullBuffer => isFullBuffer,
             
-            din => din,
+            din => dIN_s,
             dout => dout
         );
 
@@ -86,7 +106,7 @@ begin
             sp => sp,
             do_push => do_push,
             do_pop => do_pop,
-            clear => clear,
+            clear => clear_s,
             isFullBuffer => isFullBuffer,
             isEmptyBuffer => isEmptyBuffer,
             Cout => Cout,
